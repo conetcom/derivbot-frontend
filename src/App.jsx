324362837e0a34e04a4dbc6223d8d071821083bf
Wilmer Martinez <wilmerm@conetweb.com>
2026-05-24 11:33:49 -0500

    production

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import ConnectDeriv from "./pages/ConnectDeriv";
