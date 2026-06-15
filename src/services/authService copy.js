1c510d1972fe6e0c3a54c7a24ee3eb563be490b7
Wilmer Martinez <wilmerm@conetweb.com>
2026-06-14 21:08:04 -0500

    FRONTEND HOSTINGER V1.10

--- src/services/authService copy.js
+++ src/services/authService copy.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:3000/api/auth";
+const API = "/api/auth";
 
 export const login = async (data) => {
   const res = await axios.post(`${API}/login`, data);
--- src/services/botService.js
+++ src/services/botService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  "http://localhost:3000/api/bot";
+  "/api/bot";
 
 // ===============================
 // 🚀 START BOT
